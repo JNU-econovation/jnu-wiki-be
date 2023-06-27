a89bb7c708a3b1e5e1dd23e0d8c250f5dbef9141
park <pyg410@naver.com>
2023-06-27 15:54:32 +0900

    init: create project
    created jnu-wiki files
    set spring project

---
+++ src/main/java/com/timcooki/jnuwiki/JnuwikiApplication.java
@@ -0,0 +1,13 @@
+package com.timcooki.jnuwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnuwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnuwikiApplication.class, args);
+	}
+
+}
