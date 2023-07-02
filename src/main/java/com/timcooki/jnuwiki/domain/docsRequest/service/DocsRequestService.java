9f690e09ed5f38f680adfb61e5d22e37abf479a7
jminkkk <minl741@naver.com>
2023-07-02 23:04:34 +0900

    feat: create docsRequestController
    문서에 대한 사용자의 요청을 작성하는 컨트롤러으로
    writeModifiedRequest(), writeCreateRequest() 메서드 구현

---
+++ src/main/java/com/timcooki/jnuwiki/domain/docsRequest/service/DocsRequestService.java
@@ -0,0 +1,16 @@
+package com.timcooki.jnuwiki.domain.docsRequest.service;
+
+import com.timcooki.jnuwiki.domain.docsRequest.dto.CreatedRequestDto;
+import com.timcooki.jnuwiki.domain.docsRequest.dto.ModifiedRequestDto;
+import com.timcooki.jnuwiki.domain.docsRequest.entity.DocsRequest;
+
+public class DocsRequestService {
+    public DocsRequest createModifiedRequest(ModifiedRequestDto modifiedRequestDto) {
+    }
+
+    public DocsRequest createCreatedRequest(CreatedRequestDto createdRequestDto) {
+    }
+
+    public boolean hasMember(String memberId) {
+    }
+}
