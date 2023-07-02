5ba14fa678cab8a35f6181bf21398ce44aa2ee78
jminkkk <minl741@naver.com>
2023-07-03 00:31:47 +0900

    feat: create adminController
    관리자의 기능에 대한 컨트롤러로
    요청 목록 조회, 요청 승인/반려 기능 구현

--- src/main/java/com/timcooki/jnuwiki/domain/docsRequest/service/DocsRequestService.java
+++ src/main/java/com/timcooki/jnuwiki/domain/docsRequest/service/DocsRequestService.java
@@ -4,13 +4,37 @@ import com.timcooki.jnuwiki.domain.docsRequest.dto.CreatedRequestDto;
 import com.timcooki.jnuwiki.domain.docsRequest.dto.ModifiedRequestDto;
 import com.timcooki.jnuwiki.domain.docsRequest.entity.DocsRequest;
 
+import java.util.List;
+
 public class DocsRequestService {
+
     public DocsRequest createModifiedRequest(ModifiedRequestDto modifiedRequestDto) {
     }
 
     public DocsRequest createCreatedRequest(CreatedRequestDto createdRequestDto) {
     }
+    
+    public List<DocsRequest> getModifiedRequestList() {
+    }
 
-    public boolean hasMember(String memberId) {
+    public List<DocsRequest> getCreatedRequestList() {
+    }
+
+    public DocsRequest getOneModifiedRequest(String docsRequestId) {
+    }
+
+    public DocsRequest getOneCreatedRequest(String docsRequestId) {
+    }
+
+    public DocsRequest createDocsFromRequest(String docsRequestId) {
+    }
+
+    public DocsRequest updateDocsFromRequest(String docsRequestId) {
+    }
+
+    public void rejectRequest(String docsRequestId) {
+    }
+
+    public boolean hasRequest(String docsRequestId) {
     }
 }
