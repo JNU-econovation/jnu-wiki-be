749635f3fb2282eac5eb64dff96e0e72c749f31f
jminkkk <minl741@naver.com>
2023-07-09 19:46:23 +0900

    rename: Change the request WriteDTO name in detail
    요청 작성 시에 사용되는 dto 이름을 의도가 드러나게 변경

---
+++ src/main/java/com/timcooki/jnuwiki/domain/docsRequest/dto/request/CreatedRequestWriteDto.java
@@ -0,0 +1,13 @@
+package com.timcooki.jnuwiki.domain.docsRequest.dto.request;
+
+import com.timcooki.jnuwiki.domain.docs.entity.DocsLocation;
+import com.timcooki.jnuwiki.domain.docsRequest.entity.DocsCategory;
+import com.timcooki.jnuwiki.domain.docsRequest.entity.DocsRequestType;
+
+public record CreatedRequestWriteDto(
+        DocsRequestType docsRequestType,
+        DocsCategory docsRequestCategory,
+        String docsRequestName,
+        DocsLocation docsRequestLocation
+) {
+}
