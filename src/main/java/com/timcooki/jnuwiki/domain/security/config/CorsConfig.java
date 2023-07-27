0bde11e11c7834099674437cd09fd3275facd7d1
Cookie <pon05114@naver.com>
2023-07-27 17:53:45 +0900

    feat: Cors Attribution
    - Header 노출 설정 추가

--- src/main/java/com/timcooki/jnuwiki/domain/security/config/CorsConfig.java
+++ src/main/java/com/timcooki/jnuwiki/domain/security/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.timcooki.jnuwiki.domain.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,10 +11,15 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("*")
-                .allowCredentials(true)
-                .allowedHeaders("headers")
-                .maxAge(3000);
+                .allowedOrigins("*")
+                .exposedHeaders("Authorization")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.PATCH.name())
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
