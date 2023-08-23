43d3ab1a836609499a63fa9324504e970c1a9dfd
jminkkk <minl741@naver.com>
2023-08-23 14:55:31 +0900

    test: add scrap data for test
    + 임시 데이터에 스크랩 데이터 추가

--- src/main/java/com/timcooki/jnuwiki/domain/member/config/PostConstructorMember.java
+++ src/main/java/com/timcooki/jnuwiki/domain/member/config/PostConstructorMember.java
@@ -11,12 +11,15 @@ import com.timcooki.jnuwiki.domain.member.entity.Member;
 import com.timcooki.jnuwiki.domain.member.entity.MemberRole;
 import com.timcooki.jnuwiki.domain.member.repository.MemberRepository;
 import com.timcooki.jnuwiki.domain.member.service.MemberService;
+import com.timcooki.jnuwiki.domain.scrap.entity.Scrap;
+import com.timcooki.jnuwiki.domain.scrap.repository.ScrapRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
@@ -26,7 +29,7 @@ public class PostConstructorMember {
     private final DocsRepository docsRepository;
     private final PasswordEncoder passwordEncoder;
     private final DocsRequestRepository docsRequestRepository;
-
+    private final ScrapRepository scrapRepository;
     @PostConstruct
     public void init(){
 
@@ -83,7 +86,7 @@ public class PostConstructorMember {
 
         Docs docs4 = Docs.builder()
                 .docsName("업데이트4")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -95,7 +98,7 @@ public class PostConstructorMember {
 
         Docs docs5 = Docs.builder()
                 .docsName("업데이트5")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -107,7 +110,7 @@ public class PostConstructorMember {
 
         Docs docs6 = Docs.builder()
                 .docsName("업데이트X 6")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -119,7 +122,7 @@ public class PostConstructorMember {
 
         Docs docs7 = Docs.builder()
                 .docsName("업데이트X 7")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -131,7 +134,7 @@ public class PostConstructorMember {
 
         Docs docs8 = Docs.builder()
                 .docsName("업데이트X 8")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -143,7 +146,7 @@ public class PostConstructorMember {
 
         Docs docs9 = Docs.builder()
                 .docsName("업데이트X 9")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -155,7 +158,7 @@ public class PostConstructorMember {
 
         Docs docs10 = Docs.builder()
                 .docsName("업데이트 10")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -167,7 +170,7 @@ public class PostConstructorMember {
 
         Docs docs11 = Docs.builder()
                 .docsName("업데이트 11")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -179,7 +182,7 @@ public class PostConstructorMember {
 
         Docs docs12 = Docs.builder()
                 .docsName("업데이트 12")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -191,7 +194,7 @@ public class PostConstructorMember {
 
         Docs docs13 = Docs.builder()
                 .docsName("업데이트 13")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -203,7 +206,7 @@ public class PostConstructorMember {
 
         Docs docs14 = Docs.builder()
                 .docsName("업데이트X 14")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -215,7 +218,7 @@ public class PostConstructorMember {
 
         Docs docs15 = Docs.builder()
                 .docsName("업데이트 15")
-                .docsCategory(DocsCategory.nameOf("CAFE"))
+                .docsCategory(DocsCategory.CAFE)
                 .docsLocation(DocsLocation.builder()
                         .lat(35.17641341218037)
                         .lng(126.91349388159176)
@@ -234,7 +237,7 @@ public class PostConstructorMember {
                         .lng(126.91349388159176)
                         .build())
                 .docsRequestedBy(user)
-                .docsRequestCategory(DocsCategory.nameOf("CAFE"))
+                .docsRequestCategory(DocsCategory.CAFE)
                 .build();
         docsRequestRepository.save(docsRequest1);
 
@@ -246,7 +249,7 @@ public class PostConstructorMember {
                         .lng(126.91349388159176)
                         .build())
                 .docsRequestedBy(user)
-                .docsRequestCategory(DocsCategory.nameOf("CAFE"))
+                .docsRequestCategory(DocsCategory.CAFE)
                 .build();
         docsRequestRepository.save(docsRequest2);
 
@@ -258,7 +261,7 @@ public class PostConstructorMember {
                         .lng(126.91349388159176)
                         .build())
                 .docsRequestedBy(user)
-                .docsRequestCategory(DocsCategory.nameOf("CAFE"))
+                .docsRequestCategory(DocsCategory.CAFE)
                 .build();
         docsRequestRepository.save(docsRequest3);
 
@@ -300,6 +303,36 @@ public class PostConstructorMember {
                 .docs(docs1)
                 .build();
         docsRequestRepository.save(docsRequest6);
+
+        Scrap scrap1 = Scrap.builder()
+                .memberId(0L)
+                .docsId(0L)
+                .createdAt(LocalDateTime.now()).build();
+        scrapRepository.save(scrap1);
+
+        Scrap scrap2 = Scrap.builder()
+                .memberId(2L)
+                .docsId(1L)
+                .createdAt(LocalDateTime.now()).build();
+        scrapRepository.save(scrap2);
+
+        Scrap scrap3 = Scrap.builder()
+                .memberId(2L)
+                .docsId(11L)
+                .createdAt(LocalDateTime.now()).build();
+        scrapRepository.save(scrap3);
+
+        Scrap scrap4 = Scrap.builder()
+                .memberId(1L)
+                .docsId(2L)
+                .createdAt(LocalDateTime.now()).build();
+        scrapRepository.save(scrap4);
+
+        Scrap scrap5 = Scrap.builder()
+                .memberId(1L)
+                .docsId(6L)
+                .createdAt(LocalDateTime.now()).build();
+        scrapRepository.save(scrap5);
 
         //UPDATE MEMBER SET CREATED_AT = '2023-06-27 06:23:13.271379' WHERE MEMBER_ID=2
     }
