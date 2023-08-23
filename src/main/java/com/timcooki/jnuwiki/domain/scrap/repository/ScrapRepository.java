6b0074b1a01627bbaea79d2cf0645f854d6150fa
jminkkk <minl741@naver.com>
2023-08-23 21:26:01 +0900

    refactor: change dto mapping logic in repo layer to service layer
    - dto 매핑 처리를 서비스단에서 하도록 변경

---
+++ src/main/java/com/timcooki/jnuwiki/domain/scrap/repository/ScrapRepository.java
@@ -0,0 +1,11 @@
+package com.timcooki.jnuwiki.domain.scrap.repository;
+
+import com.timcooki.jnuwiki.domain.scrap.entity.Scrap;
+import com.timcooki.jnuwiki.domain.scrap.entity.ScrapId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ScrapRepository extends JpaRepository<Scrap, ScrapId> {
+    List<Scrap> findAllByMemberId(Long memberId);
+}
