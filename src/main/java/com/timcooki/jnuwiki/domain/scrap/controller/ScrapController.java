0ebe9fb0dc73feafd387553583dfdc88bea5eece
jminkkk <minl741@naver.com>
2023-09-20 00:06:37 +0900

    fix: ScrapController 응답 형식을 공통 포맷으로 변환

--- src/main/java/com/timcooki/jnuwiki/domain/scrap/controller/ScrapController.java
+++ src/main/java/com/timcooki/jnuwiki/domain/scrap/controller/ScrapController.java
@@ -3,6 +3,7 @@ package com.timcooki.jnuwiki.domain.scrap.controller;
 import com.timcooki.jnuwiki.domain.scrap.DTO.request.DeleteScrapReqDTO;
 import com.timcooki.jnuwiki.domain.scrap.DTO.request.NewScrapReqDTO;
 import com.timcooki.jnuwiki.domain.scrap.service.ScrapWriteService;
+import com.timcooki.jnuwiki.util.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,11 +19,11 @@ public class ScrapController {
 
     @PostMapping("/scrap/create")
     public ResponseEntity<?> create(@RequestBody NewScrapReqDTO newScrapReqDTO){
-        return scrapWriteService.create(newScrapReqDTO);
+        return ResponseEntity.ok(ApiUtils.success(scrapWriteService.create(newScrapReqDTO)));
     }
 
     @DeleteMapping("/scrap")
     public ResponseEntity<?> delete(@RequestBody DeleteScrapReqDTO deleteScrapReqDTO){
-        return scrapWriteService.delete(deleteScrapReqDTO);
+        return ResponseEntity.ok(ApiUtils.success(scrapWriteService.delete(deleteScrapReqDTO)));
     }
 }
