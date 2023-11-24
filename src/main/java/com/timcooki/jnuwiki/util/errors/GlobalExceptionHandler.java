d99c2aac228e050c01ed7f23c6bd0d5bb45a4552
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-24 23:12:43 +0900

    feat: 예외 발생 시 로그 출력하도록

--- src/main/java/com/timcooki/jnuwiki/util/errors/GlobalExceptionHandler.java
+++ src/main/java/com/timcooki/jnuwiki/util/errors/GlobalExceptionHandler.java
@@ -3,36 +3,42 @@ package com.timcooki.jnuwiki.util.errors;
 import com.timcooki.jnuwiki.util.ApiResult;
 import com.timcooki.jnuwiki.util.ApiUtils;
 import com.timcooki.jnuwiki.util.errors.exception.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception400.class)
     public ResponseEntity<?> badRequest(Exception400 e){
+        log.warn("[BAD REQUEST] {}", e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception401.class)
     public ResponseEntity<?> unAuthorized(Exception401 e){
+        log.warn("[UNAUTHORIZED] {}", e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception403.class)
     public ResponseEntity<?> forbidden(Exception403 e){
+        log.warn("[UNAUTHORIZED] {}", e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception404.class)
     public ResponseEntity<?> notFound(Exception404 e){
+        log.warn("[NOT FOUND] {}", e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception500.class)
     public ResponseEntity<?> serverError(Exception500 e){
+        log.error("[SERVER ERROR] {}", e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
