43308b40b20e2c98a4702a8bd2caf36de11a3b76
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-27 14:06:38 +0900

    feat: 타임존 설정

--- src/main/java/com/timcooki/jnuwiki/JnuwikiApplication.java
+++ src/main/java/com/timcooki/jnuwiki/JnuwikiApplication.java
@@ -1,5 +1,8 @@
 package com.timcooki.jnuwiki;
 
+import java.util.Date;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,8 +11,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class JnuwikiApplication {
 
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(JnuwikiApplication.class, args);
 	}
-
 }
